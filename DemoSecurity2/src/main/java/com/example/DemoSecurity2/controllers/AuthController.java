6df3d2444243e02608c3b98ff2ee57956c242ccb
TamHuy <nptamhuy93@gmail.com>
2026-04-01 10:11:01 +0700

    update demosecurity2

--- DemoSecurity2/src/main/java/com/example/DemoSecurity2/controllers/AuthController.java
+++ DemoSecurity2/src/main/java/com/example/DemoSecurity2/controllers/AuthController.java
@@ -1,10 +1,20 @@
 package com.example.DemoSecurity2.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.DemoSecurity2.dtos.UserRegistrationDTO;
+import com.example.DemoSecurity2.services.UserService;
 
 @Controller
 public class AuthController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/login")
     public String login() {
@@ -12,7 +22,19 @@ public class AuthController {
     }
 
     @GetMapping("/register")
-    public String register() {
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new UserRegistrationDTO());
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") UserRegistrationDTO registrationDto, Model model) {
+        try {
+            userService.registerNewUser(registrationDto);
+            return "redirect:/login?registered";
+        } catch (RuntimeException e) {
+            model.addAttribute("error", e.getMessage());
+            return "register";
+        }
     }
 }
---
+++ DemoSecurity2/src/main/java/com/example/DemoSecurity2/dtos/UserRegistrationDTO.java
@@ -0,0 +1,8 @@
+package com.example.DemoSecurity2.dtos;
+
+public class UserRegistrationDTO {
+    private String username;
+    private String password;
+    private String confirmPassword;
+//getter, setter, contructor
+}
