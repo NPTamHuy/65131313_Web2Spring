3ea3a94e8602fa9668f003e85dcd8c444c26b142
TamHuy <nptamhuy93@gmail.com>
2026-04-23 10:21:01 +0700

    update models

---
+++ OntapGK/src/main/java/com/example/OntapGK/OntapGkApplication.java
@@ -0,0 +1,13 @@
+package com.example.OntapGK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntapGkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OntapGkApplication.class, args);
+	}
+
+}
