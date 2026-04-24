18948ad9c78d576d9361407f65c160d0787a2d33
TamHuy <nptamhuy93@gmail.com>
2026-04-24 10:52:36 +0700

    update

---
+++ OntapGK-2/src/main/java/thigk2/nguyenphuctamhuy/ontap2/OntapGk2Application.java
@@ -0,0 +1,13 @@
+package thigk2.nguyenphuctamhuy.ontap2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntapGk2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OntapGk2Application.class, args);
+	}
+
+}
---
+++ OntapGK-2/src/test/java/thigk2/nguyenphuctamhuy/ontap2/OntapGk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk2.nguyenphuctamhuy.ontap2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntapGk2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
