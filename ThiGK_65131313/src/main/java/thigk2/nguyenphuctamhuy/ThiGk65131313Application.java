7a09ba7c9c71dba003096807b55c66e98ad1d322
TamHuy <nptamhuy93@gmail.com>
2026-04-24 13:43:22 +0700

    Lần thứ 1

---
+++ ThiGK_65131313/src/main/java/thigk2/nguyenphuctamhuy/ThiGk65131313Application.java
@@ -0,0 +1,13 @@
+package thigk2.nguyenphuctamhuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiGk65131313Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiGk65131313Application.class, args);
+	}
+
+}
